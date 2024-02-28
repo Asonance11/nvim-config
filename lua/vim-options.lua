647d71ce8335851d7b8946a4d06d21316f8938e0
Oluwadabira <adigunsamuelaso@gmail.com>
2024-02-28 22:47:17 +0100

    New config

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,6 +6,14 @@ vim.o.expandtab = true
 vim.wo.relativenumber = true
 vim.o.clipboard = 'unnamedplus'
 vim.o.completeopt = 'menuone,noselect'
+vim.o.breakindent = true
+vim.o.hlsearch = false
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.wo.signcolumn = 'yes'
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
 
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
