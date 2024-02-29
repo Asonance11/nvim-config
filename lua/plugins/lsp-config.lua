495f36710276e2dbde71157fdf7bb96275c8f659
Oluwadabira <adigunsamuelaso@gmail.com>
2024-02-29 16:53:56 +0100

    Adding python support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -102,6 +102,9 @@ return {
 			lspconfig.prismals.setup({
 				capabilities = capabilities,
 			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
