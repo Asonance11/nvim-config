0d561d2ebd03520a7b1e2c721604ae8ad57d27ad
Oluwadabira <adigunsamuelaso@gmail.com>
2024-07-09 13:01:40 +0100

    Change colorscheme

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,8 +1,14 @@
 return {
-  "luisiacc/gruvbox-baby",
+  "sainnhe/gruvbox-material",
   lazy = false,
+  name = "gruvbox-material",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("gruvbox-baby")
+    vim.cmd.colorscheme("gruvbox-material")
+    vim.g.gruvbox_material_foreground = "material"
+    --background option can be hard, medium, soft
+    vim.g.gruvbox_material_background = "dark"
+    vim.g.gruvbox_material_enable_italic = 1
+    vim.g.gruvbox_material_better_performance = 1
   end,
 }
