83e7fb858b6f925aabb1c8894f2c4dcfe9f12c4b
Oluwadabira <adigunsamuelaso@gmail.com>
2024-07-12 14:27:12 +0100

    Editoro options

---
+++ lua/vim-options.lua
@@ -0,0 +1,31 @@
+vim.g.mapleader = " "
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+vim.wo.relativenumber = true
+vim.o.completeopt = "menuone,noselect"
+vim.o.breakindent = true
+vim.o.hlsearch = false
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.wo.signcolumn = "yes"
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.clipboard = "unnamedplus"
+
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = "*",
+})
+
+-- navigating panes easier
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
