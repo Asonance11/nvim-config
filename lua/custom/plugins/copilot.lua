009932b06cceee71031d8df48d8029b19331d717
Oluwadabira <adigunsamuelaso@gmail.com>
2026-01-05 08:56:45 +0100

    Add copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,11 +1,34 @@
 return {
   'zbirenbaum/copilot.lua',
-  requires = {
-    'copilotlsp-nvim/copilot-lsp', -- (optional) for NES functionality
-  },
-  cmd = 'Copilot',
-  event = 'InsertEnter',
+  event = 'VeryLazy',
   config = function()
-    require('copilot').setup {}
+    require('copilot').setup {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        accept = false,
+      },
+      panel = {
+        enabled = false,
+      },
+      filetypes = {
+        markdown = true,
+        help = true,
+        html = true,
+        javascript = true,
+        typescript = true,
+        ['*'] = true,
+      },
+    }
+
+    vim.keymap.set('i', '<Tab>', function()
+      if require('copilot.suggestion').is_visible() then
+        require('copilot.suggestion').accept()
+      else
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, false, true), 'n', false)
+      end
+    end, {
+      silent = true,
+    })
   end,
 }
