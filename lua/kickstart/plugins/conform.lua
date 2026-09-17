11fc7e3e6167ed7d746f126a44c05ff0793fbef9
Oluwadabira <adigunsamuelaso@gmail.com>
2026-09-17 16:52:46 +0100

    dd format on save

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -4,39 +4,31 @@ local function gh(repo) return 'https://github.com/' .. repo end
 vim.pack.add { gh 'stevearc/conform.nvim' }
 require('conform').setup {
   notify_on_error = false,
-  format_on_save = function(bufnr)
-    -- You can specify filetypes to autoformat on save here:
-    local enabled_filetypes = {
-      -- lua = true,
-      -- python = true,
-    }
-    if enabled_filetypes[vim.bo[bufnr].filetype] then
-      return { timeout_ms = 500 }
-    else
-      return nil
-    end
-  end,
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_format = 'fallback',
+  },
   default_format_opts = {
     lsp_format = 'fallback', -- Use external formatters if configured below, otherwise use LSP formatting. Set to `false` to disable LSP formatting entirely.
   },
   -- You can also specify external formatters in here.
   formatters_by_ft = {
-lua = { 'stylua' },
-        css = { 'prettierd' },
-        html = { 'prettierd' },
-        json = { 'prettierd' },
-        go = { 'golines' },
+    lua = { 'stylua' },
+    css = { 'prettierd' },
+    html = { 'prettierd' },
+    json = { 'prettierd' },
+    go = { 'golines' },
 
-        -- Conform can also run multiple formatters sequentially
-        python = { 'black', 'isort' },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
-        typescript = { 'prettierd', 'prettier', stop_after_first = true },
-        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-        yaml = { 'prettierd', 'prettier', stop_after_first = true },
-        markdown = { 'prettierd', 'prettier', stop_after_first = true },
+    -- Conform can also run multiple formatters sequentially
+    python = { 'black', 'isort' },
+    --
+    -- You can use 'stop_after_first' to run the first available formatter from the list
+    javascript = { 'prettierd', 'prettier', stop_after_first = true },
+    typescript = { 'prettierd', 'prettier', stop_after_first = true },
+    javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+    typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+    yaml = { 'prettierd', 'prettier', stop_after_first = true },
+    markdown = { 'prettierd', 'prettier', stop_after_first = true },
   },
 }
 
